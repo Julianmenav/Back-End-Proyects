de8affaba57336de4446b6755278a57ac643c609
Juli <julianmenav2@gmail.com>
2021-10-18 00:08:56 +0200

    First Commit

---
+++ task-manager/app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+
+
+app.use(express.static("./public"))
+
+
+
+
+app.listen(port, console.log(`Server listening on port ${port}...`))
