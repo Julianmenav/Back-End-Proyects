0d588ecd1182c1e41ec659a3e136b63a05cffb61
Juli <julianmenav2@gmail.com>
2021-10-20 13:04:47 +0200

    Update model

--- task-manager/models/Tasks.js
+++ task-manager/models/Tasks.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type:String, 
+        required:[true, "Must provide a name for the task"],
+        trim:true,
+        maxlength: [20, "Max 20 characters"] 
+    },
+    completed: {
+        type:Boolean,
+        default:false
+    }
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
