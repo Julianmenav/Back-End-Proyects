6f6a11d7048ddfe5a1934b3d0e82040adbb18f3d
Juli <julianmenav2@gmail.com>
2021-10-22 10:32:45 +0200

    task-manager: Controller updated to fit the frontend

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -3,15 +3,16 @@ const Task = require('../models/Tasks')
 const getAllTasks = async (req, res) => {
     try {
         const tasks = await Task.find({})
-        res.status(201).json({success: true, data: tasks})
+        res.status(201).json({tasks})
     } catch (error) {
         res.status(500).json({msg:error})
     }
 }
+
 const createTask = async (req, res) => {
     try {
         const task = await Task.create(req.body)
-        res.status(201).json({success: true, data: task})
+        res.status(200).send()
     } catch (error) {
         res.status(500).json({msg: error})
     }
@@ -20,7 +21,7 @@ const getTask = async (req, res) => {
     try {
         const task = await Task.findOne({_id: req.params.id}) //.findById(req.params.id)
         if(!task) return res.status(404).json({ msg: `No task with id ${req.params.id}`})
-        res.status(201).json({success: true, data: task})
+        res.status(201).json({task})
     } catch (error) {
         res.status(500).json({msg: error})
     }
@@ -32,7 +33,7 @@ const updateTask = async (req, res) => {
             runValidators: true
         })
         if(!task) return res.status(404).json({ msg: `No task with id ${req.params.id}`})
-        res.status(200).json({success: true, data: task})
+        res.status(200).json({task})
     } catch (error) {
         res.status(500).json({msg:error})
     }
